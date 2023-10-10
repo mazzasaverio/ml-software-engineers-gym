d829dfdd58555cc1a763d71ea4367b973f71f25b
mazzasaverio <saverio3107@gmail.com>
2023-10-10 18:40:12 +0200

    ADD: dutch_flag_partition

--- 5_arrays/the_dutch_national_flag_problem.py
+++ 5_arrays/the_dutch_national_flag_problem.py
@@ -4,19 +4,14 @@
 
 
 def dutch_flag_partition(pivot_index, A):
-    print(f"pivot_index: {pivot_index}")
-    print(f"A: {A}")
     pivot = A[pivot_index]
 
     print("First pass: group elements smaller than pivot.")
     for i in range(len(A)):
         # Look for a smaller element.
         for j in range(i + 1, len(A)):
-            print(f"A[i]: {A[i]} - A[j]: {A[j]} - pivot: {pivot}")
             if A[j] < pivot:
-                print(f"condition - A[j]: {A[j]} - pivot: {pivot}")
                 A[i], A[j] = A[j], A[i]
-                print(A)
                 break
 
     print("Second pass: group elements larger than pivot.")
@@ -25,15 +20,10 @@ def dutch_flag_partition(pivot_index, A):
             break
         # Look for a larger element. Stop when we reach an element less than
         # pivot, since first pass has moved them to the start of A.
-        print(j, i)
         for j in reversed(range(i)):
-            print(f"- {j}")
-            print(f"i:{i} - j:{j} #### A[j]: {A[j]} - pivot: {pivot}")
             if A[j] > pivot:
                 A[i], A[j] = A[j], A[i]
                 break
-
-    print(f"A final: {A}")
 
 
 # Test the function
